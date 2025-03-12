1adb279e93d4f90b3d0a4c48f8692ff414f7dff7
DW Choi <ch3044637@khu.ac.kr>
2025-03-12 18:57:06 +0900

    commit

--- src/space_robots_gz/entrypoint.sh
+++ src/space_robots_gz/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 source "/opt/ros/${ROS_DISTRO}/setup.bash"
 source "${DEMO_DIR}/install/setup.bash"
 echo "alias cball='cd \${DEMO_DIR} && colcon build --parallel-workers 6 --cmake-args -DCMAKE_BUILD_TYPE=Release'" >> ~/.bashrc
-echo "alias cb='cd \${DEMO_DIR} && colcon build --packages-select canadarm mars_rover simulation --parallel-workers 4 --cmake-args -DCMAKE_BUILD_TYPE=Release'" >> ~/.bashrc
+echo "alias cb='cd \${DEMO_DIR} && colcon build --packages-select canadarm canadarm_gz_plugin mars_rover simulation --parallel-workers 4 --cmake-args -DCMAKE_BUILD_TYPE=Release'" >> ~/.bashrc
 echo "alias rl='ros2 launch canadarm canadarm.launch.py'" >> ~/.bashrc
 echo "alias rl2='ros2 launch canadarm canadarm2.launch.py'" >> ~/.bashrc
 echo "alias rl3='ros2 launch mars_rover mars_rover.launch.py'" >> ~/.bashrc
